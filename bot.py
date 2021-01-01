c1a3e708377f2c84546af9f03c6eb1b95f40808c
NiightGamez <31709198+NiightGamez@users.noreply.github.com>
2021-01-01 18:13:46 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,19 +7,35 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
-prefix = "?"
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+bot = discord.Client()
 
-@client.event
+bot = commands.Bot(command_prefix='$')
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
-@client.event
+@bot.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game('ngyt.tk/discordbot'))
+    await bot.change_presence(activity=discord.Game('github.com/niightap/discord-bot'))
 
-client.run(TOKEN)
+
+# Discord Bot Commands
+
+@bot.command()
+async def ping(ctx):
+    await ctx.channel.send("Pong")
+
+@bot.command()
+async def print(ctx, arg):
+	await ctx.channel.send(arg)    
+
+
+# help command
+
+
+
+bot.run(TOKEN)
