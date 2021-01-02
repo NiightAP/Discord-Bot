0940ecf9a15ca0c880293f254825236d3f40443a
NiightGamez <31709198+NiightGamez@users.noreply.github.com>
2021-01-02 02:08:01 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,17 +2,23 @@
 
 import os
 import dotenv
-
+import logging
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = discord.Client()
 
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix='?')
 
 @bot.event
 async def on_ready():
@@ -27,15 +33,46 @@ async def on_ready():
 
 @bot.command()
 async def ping(ctx):
-    await ctx.channel.send("Pong")
+    await ctx.channel.send(f'Pong! {round(bot.latency * 1000)}ms')
 
 @bot.command()
 async def print(ctx, arg):
-	await ctx.channel.send(arg)    
+	await ctx.channel.send(arg) 
 
+@bot.command(pass_contect = True)
+@commands.cooldown(1, 15, commands.BucketType.user)
+async def say(ctx, *args):
+    msg = ' '.join(args)
+    await ctx.message.delete()
+    return await ctx.channel.send(msg)
 
-# help command
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        await ctx.send('This command is on cooldown, you may use it again in a little bit.')
+    
+@bot.command()
+async def dev(ctx):
+    await ctx.channel.send("Developed by NiightAP: https://ngyt.tk")
 
+@bot.command()
+@commands.has_permissions(ban_members = True)
+async def ban(ctx, member : discord.Member, *, reason = None):
+    await member.ban(reason = reason)
+    await ctx.send(f':boom: Member has been banned.')
+    await ctx.message.delete()
+@bot.command()
+@commands.has_permissions(administrator = True)
+async def pardon(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split("#")
 
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if(user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unabnned {user.mention}')
+            return
 
 bot.run(TOKEN)
