95732b8ac30cc0175bb8861a15f1388a4a189bfe
Michael Voorhies <31709198+NiightGamez@users.noreply.github.com>
2021-06-25 17:16:15 -0400

    Update Moderation.py

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -6,6 +6,8 @@ class Moderation(commands.Cog):
         self.client = client
 
     # Commands
+
+    # Kick
     @commands.command()
     @commands.has_permissions(kick_members = True)
     async def kick(self, ctx, member : discord.Member, *, reason=None):
@@ -13,6 +15,7 @@ class Moderation(commands.Cog):
         await ctx.message.delete()
         await ctx.channel.send("Member has been kicked")
 
+    # Ban
     @commands.command()
     @commands.has_permissions(ban_members = True)
     async def ban(self, ctx, member : discord.Member, *, reason = None):
@@ -20,7 +23,8 @@ class Moderation(commands.Cog):
         await member.ban(reason = reason)
         await ctx.send(f':boom: Member has been banned.')
         await ctx.message.delete()
-    
+
+    # Pardon
     @commands.command()
     @commands.has_permissions(administrator = True)
     async def pardon(self, ctx, *, member):
@@ -37,11 +41,13 @@ class Moderation(commands.Cog):
                 await ctx.send(f'Unabnned {user.mention}')
                 return
 
+    # Clear
     @commands.command()
     @commands.has_permissions(administrator = True)
     async def clear(self, ctx, amount=10):
         await ctx.channel.purge(limit=amount)
 
+    # Changenick
     @commands.command(pass_context=True)
     @commands.has_permissions(administrator = True)
     async def changenick(self, ctx, member: discord.Member, nick):
@@ -49,12 +55,14 @@ class Moderation(commands.Cog):
         await member.edit(nick=nick)
         await ctx.send(f'Nickname was changed for {member.mention} ')
 
+    # Lockdown
     @commands.command()
     @commands.has_permissions(manage_channels=True)
     async def lockdown(self, ctx):
         await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False)
         await ctx.send( ctx.channel.mention + " ***Channel is now in lockdown.***")
 
+    # Unlock
     @commands.command()
     @commands.has_permissions(manage_channels=True)
     async def unlock(self, ctx):
